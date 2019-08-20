dbb4f70e6f2e35062a58b52dd6af921d50f046fe
Colin Edwards <colin@recursivepenguin.com>
2019-08-19 22:13:01 -0500

    Updating deps

--- CI/install-dependencies-osx.sh
+++ CI/install-dependencies-osx.sh
@@ -35,8 +35,8 @@ ccache -s || echo "CCache is not available."
 
 # Fetch and untar prebuilt OBS deps that are compatible with older versions of OSX
 hr "Downloading OBS deps"
-wget --quiet --retry-connrefused --waitretry=1 https://obs-nightly.s3.amazonaws.com/osx-deps-2018-08-09.tar.gz
-tar -xf ./osx-deps-2018-08-09.tar.gz -C /tmp
+wget --quiet --retry-connrefused --waitretry=1 https://obs-nightly.s3.amazonaws.com/osx-deps-2019-7-19.zip
+unzip -q -d /tmp ./osx-deps-2019-7-19.zip
 
 # Fetch vlc codebase
 hr "Downloading VLC repo"
