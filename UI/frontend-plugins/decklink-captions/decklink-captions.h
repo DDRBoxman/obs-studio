7b1a64561bc8cb56ad49c810b871046c093a4b2f
Colin Edwards <colin@recursivepenguin.com>
2020-02-21 00:49:18 -0600

    clang format my dude

--- UI/frontend-plugins/decklink-captions/decklink-captions.h
+++ UI/frontend-plugins/decklink-captions/decklink-captions.h
@@ -5,26 +5,25 @@
 #include "ui_captions.h"
 
 class DecklinkCaptionsUI : public QDialog {
-Q_OBJECT
+	Q_OBJECT
 private:
-
 public:
-    std::unique_ptr<Ui_CaptionsDialog> ui;
-    DecklinkCaptionsUI(QWidget *parent);
+	std::unique_ptr<Ui_CaptionsDialog> ui;
+	DecklinkCaptionsUI(QWidget *parent);
 
 public slots:
-    void on_source_currentIndexChanged(int idx);
+	void on_source_currentIndexChanged(int idx);
 };
 
 static inline OBSWeakSource GetWeakSourceByName(const char *name)
 {
-    OBSWeakSource weak;
-    obs_source_t *source = obs_get_source_by_name(name);
-    if (source) {
-        weak = obs_source_get_weak_source(source);
-        obs_weak_source_release(weak);
-        obs_source_release(source);
-    }
+	OBSWeakSource weak;
+	obs_source_t *source = obs_get_source_by_name(name);
+	if (source) {
+		weak = obs_source_get_weak_source(source);
+		obs_weak_source_release(weak);
+		obs_source_release(source);
+	}
 
-    return weak;
+	return weak;
 }
